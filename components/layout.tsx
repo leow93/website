eabf59633890eb38fef74c4466f8ea91701068ec
leow93 <leowheelan@hotmail.co.uk>
2022-05-04 18:03:09 +0100

    chore: tidy

---
+++ components/layout.tsx
@@ -0,0 +1,8 @@
+import styles from './layout.module.css'
+import {PropsWithChildren} from 'react'
+
+const Layout = ({children}: PropsWithChildren<any>) => {
+  return <div className={styles.container}>{children}</div>
+}
+
+export default Layout
--- todos.md
+++ todos.md
@@ -1,3 +1,3 @@
 # Todos for this site
 
-- [ ] Follow NextJS docs, here: https://nextjs.org/learn/basics/navigate-between-pages/link-component
+- [ ] Follow NextJS docs, here: https://nextjs.org/learn/basics/assets-metadata-css/polishing-layout

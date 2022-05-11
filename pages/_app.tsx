4a46a2f5db7ca063d28f9a7956cbad0c5c660b5c
leow93 <leowheelan@hotmail.co.uk>
2022-05-11 12:08:45 +0100

    fix(theme): iron out teething problems

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,41 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ThemeProvider, Theme } from '../hooks/useTheme';
+
 import Script from 'next/script';
-import { useState, useCallback, useEffect } from 'react';
-
-const useLocalStorageTheme = () => {
-  const [theme, setTheme] = useState<Theme>('light');
-
-  useEffect(() => {
-    if (window.localStorage.theme) {
-      setTheme(window.localStorage.theme);
-    }
-  }, []);
-
-  const updateTheme = useCallback((theme: Theme) => {
-    localStorage.theme = theme;
-    setTheme(theme);
-  }, []);
-
-  return [theme, updateTheme] as const;
-};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [theme, setTheme] = useLocalStorageTheme();
   return (
     <>
       <Script strategy="beforeInteractive" src="scripts/theme.js" />
-      <ThemeProvider
-        value={{
-          theme,
-          setTheme,
-        }}
-      >
-        <div className={theme}>
-          <Component {...pageProps} />
-        </div>
-      </ThemeProvider>
+      <Component {...pageProps} />
     </>
   );
 }
--- public/scripts/theme.js
+++ public/scripts/theme.js
@@ -1,9 +1,25 @@
-if (
-  localStorage.theme === 'dark' ||
-  (!('theme' in localStorage) &&
-    window.matchMedia('(prefers-color-scheme: dark)').matches)
-) {
-  document.documentElement.classList.add('dark');
-} else {
-  document.documentElement.classList.remove('dark');
+function once(fn) {
+  let run = false;
+  return function (...args) {
+    if (run) {
+      return;
+    }
+    const result = fn(...args);
+    run = true;
+    return result;
+  };
 }
+
+function bootstrapTheme() {
+  if (
+    localStorage.theme === 'dark' ||
+    (!('theme' in localStorage) &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches)
+  ) {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
+}
+
+once(bootstrapTheme)();

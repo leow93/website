130afd77778a99505758fd74e73fe648dc14e4c2
leow93 <leowheelan@hotmail.co.uk>
2022-05-22 22:00:29 +0100

    fix(page): make sure the scrolling works for overflowing content

--- components/page.tsx
+++ components/page.tsx
@@ -6,7 +6,10 @@ import nightwind from 'nightwind/helper';
 const darkBg = 'dark:bg-slate-800';
 const Layout = ({ children }: PropsWithChildren<any>) => {
   return (
-    <div className={`bg-white ${darkBg}`} style={{ height: '100vh' }}>
+    <div
+      className={`bg-white ${darkBg} overflow-scroll`}
+      style={{ height: '100vh' }}
+    >
       <div className="max-w-xl px-4 py-4 mx-auto">{children}</div>
     </div>
   );
